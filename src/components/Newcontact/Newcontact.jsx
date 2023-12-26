0d50db880ada1711ab4772c931b25f8532720328
Fynn Arthur <afynn09@gmail.com>
2023-12-26 09:27:19 +0000

    added New contact component and added required form inputs

--- src/components/Newcontact/Newcontact.jsx
+++ src/components/Newcontact/Newcontact.jsx
@@ -6,8 +6,26 @@ const Newcontact = () => {
   return (
     <div>
         <form>
+          <div>
+          <label>
+             Name
+          </label>
             <input></input>
+          </div>
+          <div>
+          <label>
+             Phone number
+          </label>
+            <input></input>
+          </div>
+          <div>
+          <label>
+             Email
+          </label>
+            <input></input>
+          </div>
         </form>
+        <button>Add Contact</button>
     </div>
   )
 }
