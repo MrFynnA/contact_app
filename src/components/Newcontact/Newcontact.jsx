f57b4f83fcf5ecfd59b12066748586d295e07105
Fynn Arthur <afynn09@gmail.com>
2023-12-26 12:10:50 +0000

    designed new contact form

--- src/components/Newcontact/Newcontact.jsx
+++ src/components/Newcontact/Newcontact.jsx
@@ -1,25 +1,32 @@
 
 
 import React from 'react'
+import Button from '../../ui_components/Button'
 
 const Newcontact = () => {
   return (
-    <div>
-        <form>
-          <div>
+    <div className='contact_form_container'>
+
+    <div className='contact_form'>
+      <h3 className='form_title'>Contact details</h3>
+        <form className='form' onSubmit={''}>
+          <div className='form_field'>
           <label>Name</label>
-            <input type='text'></input>
+            <input type='text' placeholder='enter name'></input>
           </div>
-          <div>
+          <div className='form_field'>
           <label> Phone number</label>
-            <input type='number'></input>
+            <input type='number' placeholder='enter mobile #'></input>
           </div>
-          <div>
+          <div className='form_field'>
           <label>Email</label>
-            <input type='email'></input>
+            <input type='email' placeholder='enter email'></input>
+          </div>
+          <div className='form_actions'>
+        <Button type='submit' width='100%'>Add Contact</Button>
           </div>
         </form>
-        <button type='submit'>Add Contact</button>
+    </div>
     </div>
   )
 }
--- src/ui_components/Button.jsx
+++ src/ui_components/Button.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import '../ui_components/Button.css'
 
 const Button = (props) => {
-const {className,children}=props
+const {className,children,width}=props
   return (
-<button className={'button '+ className}>{children}</button>
+<button style={{width:width}} className={'button '+ className}>{children}</button>
   )
 }
 
