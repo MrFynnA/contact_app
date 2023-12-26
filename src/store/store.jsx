35412495facd727a30d22d6e0da965f03dc765ef
Fynn Arthur <afynn09@gmail.com>
2023-12-26 14:00:07 +0000

    configured and exported  redux store to manage state

---
+++ src/store/store.jsx
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import modalSlice from "./slices/modalSlice";
+
+
+const store=configureStore({
+    reducer:{
+        modal:modalSlice
+    }
+})
+
+export default store
