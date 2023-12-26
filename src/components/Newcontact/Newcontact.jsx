19b2ed8a38d7617904853ae5799ae4db9ce746a1
Fynn Arthur <afynn09@gmail.com>
2023-12-26 02:10:23 +0000

    added New contact folder for adding new contact logic

---
+++ src/components/Newcontact/Newcontact.jsx
@@ -0,0 +1,15 @@
+
+
+import React from 'react'
+
+const Newcontact = () => {
+  return (
+    <div>
+        <form>
+            <input></input>
+        </form>
+    </div>
+  )
+}
+
+export default Newcontact
