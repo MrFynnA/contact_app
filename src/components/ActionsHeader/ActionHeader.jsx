900327ef98bb06dce4b097c477b358f8fb21c2ec
Fynn Arthur <afynn09@gmail.com>
2023-12-26 18:08:52 +0000

    changed action header style on mobile for good responsiveness

--- src/components/ActionsHeader/ActionHeader.jsx
+++ src/components/ActionsHeader/ActionHeader.jsx
@@ -3,6 +3,7 @@ import Button from '../../ui_components/Button/Button'
 import AddContactIcon from '../../assets/icons/addContact_icon'
 import { useDispatch } from 'react-redux'
 import { modalActions } from '../../store/slices/modalSlice'
+import SearchIcon from '../../assets/icons/search_icon'
 
 
 const ActionHeader = () => {
@@ -12,7 +13,7 @@ const ActionHeader = () => {
     <div className='action_header'>
         <form className='search_form'>
         <input type='text' placeholder='search contact...'></input>
-        <Button className='search_form_btn'>search</Button>
+        <Button className='search_form_btn'><span className='search_form_btn_icon'><SearchIcon/></span><span className='search_form_btn_text'>search</span></Button>
         </form>
         <Button className='action_header_btn' onClick={()=>dispatch(modalActions.setContactModalActive())}><AddContactIcon/><span className='action_header_btn_text'>Add Contact</span></Button>
     </div>
