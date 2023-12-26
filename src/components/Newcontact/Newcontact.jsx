3384c3dad51a5bafe6c8f1cc34706a5beb6b44e9
Fynn Arthur <afynn09@gmail.com>
2023-12-26 13:02:12 +0000

    used creatportal to render backdrop and modal element to the root of the body for good code structure

--- src/components/Newcontact/Newcontact.jsx
+++ src/components/Newcontact/Newcontact.jsx
@@ -3,12 +3,13 @@
 import React from 'react'
 import Button from '../../ui_components/Button/Button'
 import { Backdrop } from '../../ui_components/Backdrop/Backdrop'
+import {createPortal} from 'react-dom'
 
 const Newcontact = () => {
   return (
     <>
-    <Backdrop/>
-    <div className='contact_form_container'>
+    {createPortal(<Backdrop/>,document.getElementById('backdrop'))}
+    {createPortal(<div className='contact_form_container'>
     <div className='contact_form'>
       <h3 className='form_title'>Contact details</h3>
         <form className='form' onSubmit={''}>
@@ -29,7 +30,7 @@ const Newcontact = () => {
           </div>
         </form>
     </div>
-    </div>
+    </div>,document.getElementById('modal'))}
     </>
   )
 }
