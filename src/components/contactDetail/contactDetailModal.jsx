d784ef1c4abce800e9ea053bef9fe658fafa3c55
Fynn Arthur <afynn09@gmail.com>
2023-12-26 20:56:50 +0000

    added contail detail component to display contact details

---
+++ src/components/contactDetail/contactDetailModal.jsx
@@ -0,0 +1,27 @@
+
+
+import React from 'react'
+import { Backdrop } from '../../ui_components/Backdrop/Backdrop'
+import {createPortal} from 'react-dom'
+import { useDispatch } from 'react-redux'
+import { modalActions } from '../../store/slices/modalSlice'
+
+const Newcontact = () => {
+    const dispatch =useDispatch()
+  return (
+    <>
+    {createPortal(<Backdrop onClick={()=>dispatch(modalActions.closeContactModal())}/>,document.getElementById('backdrop'))}
+   <div id='modal'>
+    {createPortal( <div className='contact_detail'>
+         <div className='contact_detail_items'>
+            <div><span>Name:</span><span>{'Fynn Arthur'}</span></div>
+            <div><span>Number:</span><span>{'0545411925'}</span></div>
+            <div><span>Email:</span><span>{'Fynn@gmail.com'}</span></div>
+         </div>
+    </div>,document.getElementById('modal'))}
+    </div>
+    </>
+  )
+}
+
+export default Newcontact
