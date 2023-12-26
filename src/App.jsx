66de1f5289078b382513b3365cb03f790ecd32e1
Fynn Arthur <afynn09@gmail.com>
2023-12-26 15:44:39 +0000

    styled action header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Contacts from './components/Contact/contactlist'
 import Newcontact from './components/Newcontact/NewcontactForm'
 import ContactIcon from './assets/icons/contact_icon'
+import ActionHeader from './components/ActionsHeader/ActionHeader'
 function App() {
 
   return (
     <>                        
 <div className='main'>
   <header><ContactIcon/><h4>CONTACTS</h4></header>
+  <ActionHeader/>
   <Contacts/>
   {/* <Newcontact/> */}
 </div>
--- src/components/ActionsHeader/ActionHeader.jsx
+++ src/components/ActionsHeader/ActionHeader.jsx
@@ -3,15 +3,17 @@ import Button from '../../ui_components/Button/Button'
 
 const ActionHeader = () => {
   return (
+    <div className='action_container'>
     <div className='action_header'>
         <form className='search_form'>
-        <label>Search</label>
-        <input type='text'></input>
-        <Button>search</Button>
+        <input type='text' placeholder='search contact...'></input>
+        <Button className='search_form_btn'>search</Button>
         </form>
         <div>
         <Button>Add Contact</Button>
         </div>
+    </div>
+    <hr/>
     </div>
   )
 }
