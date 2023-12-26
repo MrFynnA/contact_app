c4ab3abf702be668ea815dbc6ee50e6bf3f97afe
Fynn Arthur <afynn09@gmail.com>
2023-12-26 15:47:51 +0000

    added add contact icon

---
+++ src/assets/icons/addContact_icon.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddContactIcon = () => {
+  return (
+    <svg width={'30px'} viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><g id="add_contact" data-name="add contact"><path d="m8.77 10.11a4.13 4.13 0 0 1 8.25 0 4.13 4.13 0 0 1 -8.25 0zm4.12 6.17a6.74 6.74 0 0 0 -6.73 6.72v2a1 1 0 0 0 2 0v-2a4.73 4.73 0 0 1 9.46 0v2a1 1 0 0 0 2 0v-2a6.76 6.76 0 0 0 -6.73-6.72zm12-2.08h-2v-2a1 1 0 0 0 -2 0v2h-2a1 1 0 0 0 0 2h2v2a1 1 0 1 0 2 0v-2h2a1 1 0 0 0 -.03-2z"></path></g></svg>
+  )
+}
+
+export default AddContactIcon
